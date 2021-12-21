bb7a1c04acbd6c9cc8fa74cfce415d188e4c4318
Nasir Iqbal <60297068+nasir733@users.noreply.github.com>
2021-12-21 21:25:40 +0500

    update requirements

--- api/serializers.py
+++ api/serializers.py
@@ -10,5 +10,9 @@ class BusinessSerializer(ModelSerializer):
     catergory = CatergorySerializer(many=False, read_only=True)
     class Meta:
         model = BusinessProfile
-        exclude = ('id',)
+        
+    def create(self,validated_data):
+        category = Catergory.objects.get(name=validated_data['catergory'].name)
+        business = BusinessProfile.objects.create(**validated_data, catergory=category)
+        return business
         
