2181d6c88aec293954a1e62ef948d338d01715e0
Nasir Iqbal <60297068+nasir733@users.noreply.github.com>
2021-12-24 23:08:59 +0500

    Wrote a custom script to add

--- reviews/management/commands/seedReviews.py
+++ reviews/management/commands/seedReviews.py
@@ -11,18 +11,28 @@ class Command(BaseCommand):
 
     help = "It seeds the DB with tons of stuff"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--number", default=2, type=int, help="How many reviews do you want to create?"
+        )
+        parser.add_argument("--company", type=str, default='',
+                            help="Which company do you want to seed?")
+
     def handle(self, *args, **options):
         reviews_seeder = Seed.seeder()
-        user_seeder = Seed.seeder()
-        user_seeder.add_entity(
-            User, 20, {"is_staff": False, "is_superuser": False})
-        user_seeder.execute()
-        businessProfiles = BusinessProfile.objects.all()
+        # user_seeder = Seed.seeder()
+        # user_seeder.add_entity(
+        #     User, 20, {"is_staff": False, "is_superuser": False})
+        # user_seeder.execute()
+        businessProfiles = BusinessProfile.objects.filter(
+            name=options.get('company')).first()
         users = User.objects.all()
-        reviews_seeder.add_entity(Review, 100, {
+        reviews_seeder.add_entity(Review, options.get("number"), {
             'business': lambda x: random.choice(businessProfiles),
             'user': lambda x: random.choice(users),
-  
+
+            'review': lambda x: " ".join(),
+            'content': lambda x: " ".join(),
             'rating': lambda x: random.randint(3, 5),
 
             # 'nickname': lambda x: reviews_seeder.faker.email(),
