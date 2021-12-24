2a930878c26cc96dfca5ded58a21a84d317c6952
Nasir Iqbal <60297068+nasir733@users.noreply.github.com>
2021-12-24 18:07:59 +0500

    Wrote a custom script to add

--- reviews/management/commands/seedReviews.py
+++ reviews/management/commands/seedReviews.py
@@ -2,8 +2,9 @@ import random
 from datetime import datetime
 from django.core.management.base import BaseCommand
 from django_seed import Seed
-from users.models import User
+from users.models import CustomUser as User
 from reviews.models import Review, ReviewComment
+from companydashboard.models import BusinessProfile
 
 
 class Command(BaseCommand):
@@ -16,17 +17,18 @@ class Command(BaseCommand):
         user_seeder.add_entity(
             User, 20, {"is_staff": False, "is_superuser": False})
         user_seeder.execute()
-
+        businessProfiles = BusinessProfile.objects.all()
         users = User.objects.all()
         reviews_seeder.add_entity(Review, 100, {
-            'business': lambda x: random.randint(0, 1000),
-            'user': lambda x: random.randint(0, 2),
-            'review': lambda x: random.randint(0, 1000),
-            'content': lambda x: random.randint(0, 1000),
-            'rating': lambda x: reviews_seeder.faker.catch_phrase(),
+            'business': lambda x: random.choice(businessProfiles),
+            'user': lambda x: random.choice(users),
+            'review': lambda x: reviews_seeder.faker.catch_phrase(),
+            'content': lambda x: reviews_seeder.faker.catch_phrase(),
+            'rating': lambda x: random.randint(3, 5),
 
             # 'nickname': lambda x: reviews_seeder.faker.email(),
 
         })
+        reviews_seeder.execute()
 
         self.stdout.write(self.style.SUCCESS(f"Everything seeded"))
